59d9bd5c5513c63528588a4cbde214af0f793524
zhangfan <13653562450@163.com>
2017-08-26 17:56:53 +0800

    add IPictureBase

---
+++ app/src/main/java/com/zf/myapplication/struct/picture/IPictureBase.java
@@ -0,0 +1,13 @@
+package com.zf.myapplication.struct.picture;
+
+import android.widget.ImageView;
+
+/**
+ * creater: zf
+ * qq: 2033152950
+ * time:2017/8/26 0026 下午 5:26
+ */
+
+public interface IPictureBase {
+    public void loadImage(Object object, String url, ImageView view);
+}
